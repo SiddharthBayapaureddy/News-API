8e60274e0f9a8d427eb978b8ccc121931e95ca38
Siddharth <siddharthbayapureddy@gmail.com>
2025-01-20 14:55:17 +0530

    Added links next to news text, limit news to 15, added subject, and made the news url more accessible

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import requests
 from send_mail import send_mail
 
 api_key = '3b319c643ace403e9d87dbd08f2fb587'
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-12-20&sortBy=publishedAt&apiKey=3b319c643ace403e9d87dbd08f2fb587'
+
+topic = 'tesla'
+
+url =   "https://newsapi.org/v2/everything?" \
+        f"q={topic}&" \
+        "from=2024-12-20&" \
+        "sortBy=publishedAt&" \
+        "language=en&" \
+        "apiKey=3b319c643ace403e9d87dbd08f2fb587" \
 
 # Making a request
 request = requests.get(url)
@@ -11,17 +19,20 @@ request = requests.get(url)
 content = request.json()
 
 # Accessing the articles and discription 
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
-    print('\n')
+# for article in content['articles']:
+#     print(article['title'])
+#     print(article['description'])
+#     print('\n')
+
 
 # Making the text for mail
-
-text = ""
-for article in content['articles']:
+text = "Subject: Today's news " + '\n'
+for article in content['articles'][:15]:
     if article['title'] != None and article['description'] != None:
-        text = text + article['title'] + '\n' + article['description'] + '\n' + '\n'
+        text = text + article['title'] + '\n' + article['description'] + '\n' + article['url'] + '\n' + '\n'
 
+# Encoding to UTF-8
 text = text.encode('utf-8')
+
+# Bringing the function
 send_mail(text)
